3ca7c0440a12db26aab761494d1920b0ebbd6897
David Grammatico <david.lugdunum@gmail.com>
2024-10-23 20:56:15 +0200

    add nav and spartan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, HlmButtonDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'apprendre-angular';
+  title = 'Apprendre Angular';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ReactiveFormsComponent } from './pages/reactive-forms/reactive-forms.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Apprendre Angular',
+    component: HomeComponent,
+  },
+  {
+    path: 'reactive-forms',
+    title: 'Reactive Forms',
+    component: ReactiveFormsComponent,
+  },
+];
