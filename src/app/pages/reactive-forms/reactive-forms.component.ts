dadee26c5a3396790674b4415730f677d7f1023f
David Grammatico <david.lugdunum@gmail.com>
2024-10-24 12:09:11 +0200

    add ReactFormModule and logic for FormGroup

--- src/app/pages/reactive-forms/reactive-forms.component.ts
+++ src/app/pages/reactive-forms/reactive-forms.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   standalone: true,
-  imports: [HlmButtonDirective],
+  imports: [HlmButtonDirective, ReactiveFormsModule],
   templateUrl: './reactive-forms.component.html',
   styleUrl: './reactive-forms.component.css',
 })
-export class ReactiveFormsComponent {}
+export class ReactiveFormsComponent {
+  protected readonly formSignup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    repeatPassword: new FormControl('', Validators.required),
+  });
+
+  handleSubmit() {
+    console.log(this.formSignup.value);
+  }
+}
